922401e04a84512c5e993920958d6c03b46d503a
Владислав Тригуб <vtryhub@portmone.local>
2024-05-27 20:34:38 +0300

    secondTask

--- dz-6/src/main/java/org/example/App.java
+++ dz-6/src/main/java/org/example/App.java
@@ -1,39 +1,43 @@
 package org.example;
-
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        ArrayList<String> carBrands = new ArrayList<>();
-        carBrands.add("Toyota");
+        List<String> carBrands = new ArrayList<>();
+        carBrands.add("Tesla");
         carBrands.add("Ford");
+        carBrands.add("Audi");
+        carBrands.add("Toyota");
         carBrands.add("BMW");
-        carBrands.add("Mercedes");
-        carBrands.add("Honda");
+        carBrands.add("Nissan");
+        carBrands.add("BMW");
 
-
-        String searchString = "BMW";
-
-        boolean result = contains(carBrands, searchString);
-        System.out.println("Наявність: " + searchString + " -> " + result);
+        removeDouble(carBrands);
     }
 
-    public static boolean contains(ArrayList<String> list, String target) {
-        System.out.print("Марки автомобілів: ");
-        for (String element : list) {
-            System.out.print("| " +element + " | ");
-        }
-        System.out.println();
+    public static void removeDouble(List<String> list) {
+        List<String> uniqueCarBrands = new ArrayList<>();
 
-        for (String element : list) {
-            if (element.equals(target)) {
-                return true;
+        for (String brand : list) {
+            if (!uniqueCarBrands.contains(brand)) {
+                uniqueCarBrands.add(brand);
             }
         }
 
-        return false;
+        System.out.println("Список авто: " + list +"\n");
+
+        System.out.println("Результат після змін:");
+        boolean isFirst = true;
+        for (String brand : uniqueCarBrands) {
+            if (!isFirst) {
+                System.out.print(", ");
+            } else {
+                isFirst = false;
+            }
+            System.out.print(brand);
+        }
     }
 
 }
